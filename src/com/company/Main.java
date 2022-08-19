ec28dcd8c92a2313309c03152fcbb606de854b09
Wiik Candido Maria <wiikcandido.maria@syk.fi>
2022-08-19 10:20:11 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int a = 100;
+        int b = 51;
+        int f;
+
+        if (a >= 100 && b <= 50)
+            f = 1;
+        else
+            f = 0;
+        System.out.println(f);
+
+    }
+}
